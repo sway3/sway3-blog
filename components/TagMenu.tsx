45c4c0dbb7e8fb3d77b6cee838ccc075437aabda
Seungwoo Kim <svvoo31o@gmail.com>
2024-06-26 16:10:34 +0100

    Refactor: Tag menu component changed to details tag

--- components/TagMenu.tsx
+++ components/TagMenu.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { useState } from 'react';
 import IconWithText from './common/IconWithText';
 import TagBox from './common/TagBox';
 import CaretRight from '@/public/icons/CaretRight';
@@ -10,27 +7,21 @@ interface TagMenuProps {
 }
 
 export default function TagMenu({ tags }: TagMenuProps) {
-  const [tagOpen, isTagOpen] = useState(false);
-
   return (
-    <>
-      <div className="flex">
-        <div onClick={() => isTagOpen(!tagOpen)}>
-          <IconWithText
-            Icon={CaretRight}
-            iconSize={16}
-            text="Tags"
-            className="cursor-pointer rounded-lg border p-1.5 pr-2 text-lg"
-          />
-        </div>
+    <details className="flex [&_svg]:open:rotate-90 [&_svg]:open:transition [&_svg]:open:duration-300 [&_svg]:open:ease-in-out">
+      <summary className="w-fit list-none">
+        <IconWithText
+          Icon={CaretRight}
+          iconSize={16}
+          text="Tags"
+          className="cursor-pointer flex-row-reverse rounded-lg border p-1.5 pr-2 text-lg"
+        />
+      </summary>
+      <div className="mt-2 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <TagBox key={tag} tag={tag} />
+        ))}
       </div>
-      <div className={`mb-3 ${tagOpen ? 'block' : 'hidden'}`}>
-        <div className="mt-2 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <TagBox key={tag} tag={tag} />
-          ))}
-        </div>
-      </div>
-    </>
+    </details>
   );
 }
